f33ad198f279736cf5d08782d45457ef657ff3e4
Obaidul1545 <mdobaidulislam128@gmail.com>
2025-11-10 22:13:01 +0600

    firebase email and password authentication added

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,14 +1,34 @@
 import { Eye, EyeOff, LogIn } from 'lucide-react';
 import { use, useState } from 'react';
-import { Link } from 'react-router';
+import { Link, useLocation, useNavigate } from 'react-router';
 import { AuthContext } from '../../Context/AuthContext';
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const { signInGoogle } = use(AuthContext);
+  const { signInGoogle, signIn } = use(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  const handleSignInGoogle = () => {
-    console.log('click');
+  const handleLogIn = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    signIn(email, password)
+      .then(() => {
+        // toast.success('Login Successful!');
+        e.target.reset();
+        navigate(`${location.state ? location.state : '/'}`);
+      })
+      .catch((error) => {
+        // toast.error(error.message);
+        console.log(error);
+      });
+  };
+
+  const handleSignInGoogle = (e) => {
+    e.preventDefault();
     signInGoogle()
       .then((result) => {
         console.log(result.user);
@@ -33,86 +53,85 @@ const Login = () => {
 
         <div className="card bg-base-100 w-full shrink-0 shadow-2xl mt-5">
           <div className="card-body md:w-xl">
-            <fieldset className="fieldset">
-              <label className="label font-semibold mt-2" name="email">
-                Email
-              </label>
-              <input
-                type="email"
-                className="input w-full"
-                placeholder="Enter Your Email"
-              />
-              <div className="relative">
-                <label
-                  className="label font-semibold mt-2 mb-1"
-                  name="password"
-                >
-                  Password
-                </label>
+            <form onSubmit={handleLogIn}>
+              <fieldset className="fieldset">
+                <label className="label font-semibold mt-2">Email</label>
                 <input
-                  type={showPassword ? 'text' : 'password'}
-                  className="input w-full "
-                  placeholder="Password"
+                  type="email"
+                  className="input w-full"
+                  placeholder="Enter Your Email"
+                  name="email"
                 />
-                <button
-                  className="absolute top-10.5 right-4 z-10"
-                  type="button"
-                  onClick={() => setShowPassword(!showPassword)}
-                >
-                  {showPassword ? (
-                    <EyeOff size={18} className="text-gray-600" />
-                  ) : (
-                    <Eye size={18} className="text-gray-600" />
-                  )}{' '}
+                <div className="relative">
+                  <label className="label font-semibold mt-2 mb-1">
+                    Password
+                  </label>
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    className="input w-full "
+                    placeholder="Password"
+                    name="password"
+                  />
+                  <button
+                    className="absolute top-10.5 right-4 z-10"
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? (
+                      <EyeOff size={18} className="text-gray-600" />
+                    ) : (
+                      <Eye size={18} className="text-gray-600" />
+                    )}{' '}
+                  </button>
+                </div>
+                <div>
+                  <a className="link link-hover hover:text-[#FF6B6B] ">
+                    Forgot password?
+                  </a>
+                </div>
+                <button className="btn bg-[#FF6B6B]  text-white mt-4">
+                  Login
                 </button>
-              </div>
-              <div>
-                <a className="link link-hover hover:text-[#FF6B6B] ">
-                  Forgot password?
-                </a>
-              </div>
-              <button className="btn bg-[#FF6B6B]  text-white mt-4">
-                Login
-              </button>
-              <div className="flex justify-center text-sm my-4">
-                <span className="px-4 bg-white text-gray-500">
-                  Or continue with
-                </span>
-              </div>
-              <button
-                onClick={handleSignInGoogle}
-                className="btn bg-white text-black border-[#e5e5e5]"
-              >
-                <svg
-                  aria-label="Google logo"
-                  width="16"
-                  height="16"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
+                <div className="flex justify-center text-sm my-4">
+                  <span className="px-4 bg-white text-gray-500">
+                    Or continue with
+                  </span>
+                </div>
+                <button
+                  onClick={handleSignInGoogle}
+                  className="btn bg-white text-black border-[#e5e5e5]"
                 >
-                  <g>
-                    <path d="m0 0H512V512H0" fill="#fff"></path>
-                    <path
-                      fill="#34a853"
-                      d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
-                    ></path>
-                    <path
-                      fill="#4285f4"
-                      d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
-                    ></path>
-                    <path
-                      fill="#fbbc02"
-                      d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
-                    ></path>
-                    <path
-                      fill="#ea4335"
-                      d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
-                    ></path>
-                  </g>
-                </svg>
-                Login with Google
-              </button>
-            </fieldset>
+                  <svg
+                    aria-label="Google logo"
+                    width="16"
+                    height="16"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  >
+                    <g>
+                      <path d="m0 0H512V512H0" fill="#fff"></path>
+                      <path
+                        fill="#34a853"
+                        d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                      ></path>
+                      <path
+                        fill="#4285f4"
+                        d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                      ></path>
+                      <path
+                        fill="#fbbc02"
+                        d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                      ></path>
+                      <path
+                        fill="#ea4335"
+                        d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                      ></path>
+                    </g>
+                  </svg>
+                  Login with Google
+                </button>
+              </fieldset>
+            </form>
 
             <p className="mt-6 text-center text-sm text-gray-600">
               Don't have an account?{' '}
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,10 +1,57 @@
 import { Eye, EyeOff, UserPlus } from 'lucide-react';
-import React, { useState } from 'react';
+import React, { use, useState } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../../Context/AuthContext';
 
 const Register = () => {
+  const { signInGoogle, createUser } = use(AuthContext);
   const [showPassword, setShowPassword] = useState(false);
+  const [nameError, setNameError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
+  const handleCreateUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    if (name.length < 5) {
+      setNameError('Name should be more then 5 charecter');
+      return;
+    } else {
+      setNameError('');
+    }
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const passRex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-={}[\]|:;"'<>,.?/]).{6,}$/;
+    if (!passRex.test(password)) {
+      setPasswordError(
+        'Password must contain at least one uppercase letter, one lowercase letter, one special character, and be at least 6 characters long.'
+      );
+      return;
+    } else {
+      setPasswordError('');
+    }
+    const photoUrl = e.target.photo.value;
+
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log('create success');
+      })
+      .then((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSignInGoogle = (e) => {
+    e.preventDefault();
+    signInGoogle()
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="flex justify-center mt-10">
       <div>
@@ -22,95 +69,99 @@ const Register = () => {
 
         <div className="card bg-base-100 w-full shrink-0 shadow-2xl mt-5">
           <div className="card-body md:w-xl">
-            <fieldset className="fieldset">
-              <label className="label font-semibold mt-2" name="email">
-                Full Name
-              </label>
-              <input
-                type="text"
-                className="input w-full"
-                placeholder="Enter Your Full Name"
-              />
-              <label className="label font-semibold mt-2" name="email">
-                Email
-              </label>
-              <input
-                type="email"
-                className="input w-full"
-                placeholder="Enter Your Email"
-              />
-              <div className="relative">
-                <label
-                  className="label font-semibold mt-2 mb-1"
-                  name="password"
-                >
-                  Password
-                </label>
+            <form onSubmit={handleCreateUser}>
+              <fieldset className="fieldset">
+                <label className="label font-semibold mt-2">Full Name</label>
                 <input
-                  type={showPassword ? 'text' : 'password'}
-                  className="input w-full "
-                  placeholder="Password"
+                  type="text"
+                  className="input w-full"
+                  placeholder="Enter Your Full Name"
+                  name="name"
                 />
-                <button
-                  className="absolute top-10.5 right-4 z-10"
-                  type="button"
-                  onClick={() => setShowPassword(!showPassword)}
-                >
-                  {showPassword ? (
-                    <EyeOff size={18} className="text-gray-600" />
-                  ) : (
-                    <Eye size={18} className="text-gray-600" />
-                  )}{' '}
-                </button>
-              </div>
-              <label className="label font-semibold mt-2" name="email">
-                Photo URL
-              </label>
-              <input
-                type="email"
-                className="input w-full"
-                placeholder="Enter Your Photo URL"
-              />
+                {<p className="text-red-400 my-1">{nameError}</p>}
+                <label className="label font-semibold mt-2">Email</label>
+                <input
+                  type="email"
+                  className="input w-full"
+                  placeholder="Enter Your Email"
+                  name="email"
+                />
+                <label className="label font-semibold mt-2">Photo URL</label>
+                <input
+                  type="text"
+                  className="input w-full"
+                  placeholder="Enter Your Photo URL"
+                  name="photo"
+                />
+                <div className="relative">
+                  <label className="label font-semibold mt-2 mb-1">
+                    Password
+                  </label>
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    className="input w-full "
+                    placeholder="Password"
+                    name="password"
+                  />
+                  {passwordError && (
+                    <p className="text-red-400 mt-2">{passwordError}</p>
+                  )}
+                  <button
+                    className="absolute top-10.5 right-4 z-10"
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? (
+                      <EyeOff size={18} className="text-gray-600" />
+                    ) : (
+                      <Eye size={18} className="text-gray-600" />
+                    )}{' '}
+                  </button>
+                </div>
 
-              <button className="btn bg-[#00B894]  text-white mt-4">
-                Register
-              </button>
-              <div className="flex justify-center text-sm my-4">
-                <span className="px-4 bg-white text-gray-500">
-                  Or continue with
-                </span>
-              </div>
-              <button className="btn bg-white text-black border-[#e5e5e5]">
-                <svg
-                  aria-label="Google logo"
-                  width="16"
-                  height="16"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
+                <button className="btn bg-[#00B894]  text-white mt-4">
+                  Register
+                </button>
+                <div className="flex justify-center text-sm my-4">
+                  <span className="px-4 bg-white text-gray-500">
+                    Or continue with
+                  </span>
+                </div>
+                <button
+                  onClick={handleSignInGoogle}
+                  className="btn bg-white text-black border-[#e5e5e5]"
                 >
-                  <g>
-                    <path d="m0 0H512V512H0" fill="#fff"></path>
-                    <path
-                      fill="#34a853"
-                      d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
-                    ></path>
-                    <path
-                      fill="#4285f4"
-                      d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
-                    ></path>
-                    <path
-                      fill="#fbbc02"
-                      d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
-                    ></path>
-                    <path
-                      fill="#ea4335"
-                      d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
-                    ></path>
-                  </g>
-                </svg>
-                Login with Google
-              </button>
-            </fieldset>
+                  <svg
+                    aria-label="Google logo"
+                    width="16"
+                    height="16"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  >
+                    <g>
+                      <path d="m0 0H512V512H0" fill="#fff"></path>
+                      <path
+                        fill="#34a853"
+                        d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                      ></path>
+                      <path
+                        fill="#4285f4"
+                        d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                      ></path>
+                      <path
+                        fill="#fbbc02"
+                        d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                      ></path>
+                      <path
+                        fill="#ea4335"
+                        d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                      ></path>
+                    </g>
+                  </svg>
+                  Login with Google
+                </button>
+              </fieldset>
+            </form>
 
             <p className="mt-6 text-center text-sm text-gray-600">
               Already have an account?{' '}
